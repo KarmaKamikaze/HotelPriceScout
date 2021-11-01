e22e9636cad1f4232ce9bde27bebe3d93dbf9850
KarmaKamikaze <nicolaijgn@gmail.com>
2021-11-01 11:34:27 +0100

    Booking site created, missing methods dependant on other classes

---
+++ HotelPriceScout/Data/Model/BookingSite.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace HotelPriceScout.Data.Model
+{
+    public class BookingSite
+    {
+        private readonly string _name;
+        private readonly string _type;
+
+        public BookingSite(string name, string type, string url)
+        {
+            try
+            {
+                Name = name ?? throw new ArgumentNullException();
+                Type = type ?? throw new ArgumentNullException();
+                Url = url ?? throw new ArgumentNullException();
+                // TODO: Initialize list of hotels.
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            init { if (value != null) _name = value; }
+        }
+
+        public string Type
+        {
+            get => _type;
+            init
+            {
+                if (value is "single" or "multi") _type = value;
+                else throw new ArgumentOutOfRangeException(
+                    $"{nameof(value)} must be either \"single\" or \"multi\".");
+            } 
+        }
+        
+        // TODO: List property, containing hotels.
+
+        public string Url { get; }
+
+        private void CreateHotel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreatePriceScraper()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
