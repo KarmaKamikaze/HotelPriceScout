9b91cb43f13bcb1ac3252b19cecdf44f943b5ec3
KarmaKamikaze <nicolaijgn@gmail.com>
2021-11-03 14:27:25 +0100

    Selects HotelName instead of * in LoadStaticHotelResources

--- DataAccessLibrary/SqliteDataAccess.cs
+++ DataAccessLibrary/SqliteDataAccess.cs
@@ -26,13 +26,8 @@ namespace DataAccessLibrary
         public async Task<IEnumerable<string>> LoadStaticHotelResources(string group)
         {
             using IDbConnection connection = new SQLiteConnection(LoadConnectionString());
-            IEnumerable<string> output = await connection.QueryAsync<string>($"select * from StaticData WHERE {group.ToLower()} = TRUE", new DynamicParameters());
+            IEnumerable<string> output = await connection.QueryAsync<string>($"select HotelName from StaticData WHERE {group.ToLower()} = TRUE", new DynamicParameters());
             IEnumerable<string> resources = output.ToList();
-
-            foreach (var item in resources)
-            {
-                Console.WriteLine(item);
-            }
 
             return resources;
         }
