23ae8cb002c1b5b467660fcc2767b9859ee51788
KarmaKamikaze <nicolaijgn@gmail.com>
2021-11-04 11:28:07 +0100

    Possible to load static booking site data from db

--- DataAccessLibrary/SqliteDataAccess.cs
+++ DataAccessLibrary/SqliteDataAccess.cs
@@ -26,8 +26,31 @@ namespace DataAccessLibrary
         public async Task<IEnumerable<string>> LoadStaticHotelResources(string group)
         {
             using IDbConnection connection = new SQLiteConnection(LoadConnectionString());
-            IEnumerable<string> output = await connection.QueryAsync<string>($"select HotelName from StaticData WHERE {group.ToLower()} = TRUE", new DynamicParameters());
+            IEnumerable<string> output = await connection.QueryAsync<string>(
+                $"select HotelName from StaticDataHotels WHERE {group.ToLower()} = TRUE", new DynamicParameters());
             IEnumerable<string> resources = output.ToList();
+
+            return resources;
+        }
+
+        public async Task<List<(string, string, string, Dictionary<string, string>)>> LoadStaticBookingSiteResources()
+        {
+            using IDbConnection connection = new SQLiteConnection(LoadConnectionString());
+            IEnumerable<(string name, string type, string url, string hotels)> output = await connection.QueryAsync<(string, string, string, string)>("select * from StaticDataBookingSites", new DynamicParameters());
+
+            var resources = new List<(string, string, string, Dictionary<string, string>)>();
+
+            foreach ((string name, string type, string url, string hotels) item in output)
+            {
+                var hotelDict = new Dictionary<string, string>();
+                string[] hotelNameAndTag = item.hotels.Split(';');
+                for (int k = 0; k < hotelNameAndTag.Length; k++)
+                {
+                    string[] hotelNameAndTagDivided = hotelNameAndTag[k].Split(':');
+                    hotelDict.Add(hotelNameAndTagDivided[0], hotelNameAndTagDivided[1]);
+                }
+                resources.Add((item.name, item.type, item.url, hotelDict));
+            }
 
             return resources;
         }
