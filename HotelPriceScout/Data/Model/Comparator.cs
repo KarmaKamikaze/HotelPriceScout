6319058e65451b94f8608a48ca30a71addc71e4e
Sarmisuper <sarmilan3@gmail.com>
2021-11-08 13:16:41 +0100

    Compareprices method completed but not tested yet. It can maybe be optimized also but we will see

--- HotelPriceScout/Data/Model/Comparator.cs
+++ HotelPriceScout/Data/Model/Comparator.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace HotelPriceScout.Data.Model
 {
@@ -33,7 +35,65 @@ namespace HotelPriceScout.Data.Model
 
         public void ComparePrices()
         {
-            throw new NotImplementedException();
+            Dictionary<DateTime, Dictionary<string, decimal>> roomtype1HotelAvgPrices = new();
+            Dictionary<DateTime, Dictionary<string, decimal>> roomtype2HotelAvgPrices = new();
+            Dictionary<DateTime, Dictionary<string, decimal>> roomtype3HotelAvgPrices = new();
+            Dictionary<DateTime, List<decimal>> marketPrices = new();
+
+
+            for (DateTime date = DateTime.Now; date < date.AddMonths(3); date = date.AddDays(1))
+            {
+                Dictionary<string, decimal> dict1 = new();
+                Dictionary<string, decimal> dict2 = new();
+                Dictionary<string, decimal> dict3 = new();
+
+                //The integer describes the capacity of the roomtypes.
+                List<(Dictionary<string, decimal>, int)> dictList = new();
+                dictList.Add((dict1, 1));
+                dictList.Add((dict2, 2));
+                dictList.Add((dict3, 4));
+
+                foreach (BookingSite bookingSite in BookingSites)
+                {
+                    foreach (Hotel hotel in bookingSite.HotelsList)
+                    {
+                        foreach ((Dictionary<string, decimal> dict, int capacity) in dictList)
+                        {
+                            RoomType roomType = hotel.RoomTypes.Where(r => r.Capacity == capacity).First();
+
+                            RoomTypePrice roomTypePrice = roomType.Prices.Find(p => p.Date == date);
+
+                            if (roomTypePrice != null)
+                            {
+                                if (dict.ContainsKey(hotel.Name))
+                                {
+                                    dict[hotel.Name] = (dict[hotel.Name] + roomTypePrice.Price) / 2;
+                                }
+                                else
+                                {
+                                    dict.Add(hotel.Name, roomTypePrice.Price);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                roomtype1HotelAvgPrices.Add(date, dict1);
+                roomtype2HotelAvgPrices.Add(date, dict2);
+                roomtype3HotelAvgPrices.Add(date, dict3);
+                List<decimal> list = new();
+                foreach ((Dictionary<string, decimal> dict, int capacity) in dictList)
+                {
+                    decimal accumulatedPrice = 0;
+                    foreach(KeyValuePair<string, decimal> keyValuePair in dict)
+                    {
+                        accumulatedPrice += keyValuePair.Value;
+                    }
+                    
+                    list.Add(accumulatedPrice / dict.Count());
+                }
+                marketPrices.Add(date, list);
+            }
         }
 
         public void SendNotification()
