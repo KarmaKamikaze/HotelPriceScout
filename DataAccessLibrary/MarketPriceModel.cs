e7ddff3b3a25bb983defdb7294c67f93634743fe
Tobias <tlaust18@student.aau.dk>
2021-11-10 13:04:50 +0100

    Fixed MarketPrice for Kompas Price Day Setter

--- DataAccessLibrary/MarketPriceModel.cs
+++ DataAccessLibrary/MarketPriceModel.cs
@@ -6,11 +6,19 @@ namespace HotelPriceScout.Data.Model
         public int Price { get; set; }
         public DateTime Date { get; set; }
 
+        public string HotelName { get; set;  }
+
+        public MarketPriceModel(string hotelname, int price, DateTime date)
+        {
+            Price = price;
+            Date = date;
+            HotelName = hotelname;
+        }
+
         public MarketPriceModel(int price, DateTime date)
         {
             Price = price;
             Date = date;
-
         }
     }
 }
