9981f3a6fcd0a66fd7729884157e39c524d8fbba
Mrtobs <tlaust18@student.aau.dk>
2021-11-11 10:01:30 +0100

    Inserted stashed Unit section

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class RENAME_THIS_TEST_CLASS
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
