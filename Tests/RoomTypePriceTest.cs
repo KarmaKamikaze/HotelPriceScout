341965b5fddead20168abb6bbe2e14a0d2c907f7
Sarmisuper <sarmilan3@gmail.com>
2021-11-15 15:39:49 +0100

    room type price test initial work

---
+++ Tests/RoomTypePriceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelPriceScout.Data.Model;
+using Xunit;
+
+namespace Tests
+{
+    public class RoomTypePriceTest
+    {
+
+        [Fact]
+        public void DateSetterThrowsWhenValueIsBeforeCurrentDate()
+        {
+            //Arrange
+            DateTime date = DateTime.Now.AddDays(-1);
+            void Action()
+            {
+                RoomTypePrice roomTypePrice = new RoomTypePrice(date);
+            }
+
+            //Act and Assert
+            Assert.Throws<ArgumentOutOfRangeException>(Action);
+
+        }
+
+    }
+}
