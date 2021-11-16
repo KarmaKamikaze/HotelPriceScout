9543dc6bc6b08c4b433a028329a0cdd876c8784f
Mrtobs <tlaust18@student.aau.dk>
2021-11-16 11:33:01 +0100

    First unit test done. Will do rest later

--- Tests/RoomTypeTest.cs
+++ Tests/RoomTypeTest.cs
@@ -1,26 +1,44 @@
 using System;
 using Xunit;
+using HotelPriceScout.Data.Model;
 
 namespace Tests
 {
     public class RoomTypeTest
     {
-        [InlineData(1)]
-        [InlineData(2)]
+        [InlineData(-33)]
+        [InlineData(3)]
+        [InlineData(124)]
         [Theory]
-        public void Test_Capacity_For_Validity_One(int value)
+        public void Test_Capacity_For_Validity_Throws(int value)
         {
-            void SetCapacity(int value)
+            //Arrange and act
+            void Action()
             {
-
+                RoomType roomType = new RoomType(value);
             }
 
+            Assert.Throws<ArgumentOutOfRangeException>(Action);
 
-            Assert.Equal(1, value);
-            Assert.Throws<ArgumentOutOfRangeException>(() => SetCapacity(value));
+        }
+
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [Theory]
+        public void Test_Capacity_For_Validity_RoomType(int value)
+        {
+            //Arrange and act
+            void Action(int value)
+            {
+                RoomType roomType = new RoomType(value);
+            }
+
+            Assert.True(Action(value));
+            
+
         }
 
 
-
-    } 
+    }
 }
