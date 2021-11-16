36b18d4382b7da42e5402c4455e9162f1ee7177b
Sarmisuper <sarmilan3@gmail.com>
2021-11-16 09:27:47 +0100

    roomtypeprice tests er done

--- Tests/RoomTypePriceTest.cs
+++ Tests/RoomTypePriceTest.cs
@@ -12,7 +12,7 @@ namespace Tests
     {
 
         [Fact]
-        public void DateSetterThrowsWhenValueIsBeforeCurrentDate()
+        public void DateSetterThrowsWhenValueIsBeforeCurrentDateTest()
         {
             //Arrange
             DateTime date = DateTime.Now.AddDays(-1);
@@ -23,7 +23,49 @@ namespace Tests
 
             //Act and Assert
             Assert.Throws<ArgumentOutOfRangeException>(Action);
+        }
 
+        [Fact]
+        public void DateSetterCorrectlyAssignsValueTest()
+        {
+            //Arrange
+            DateTime date = DateTime.Now;
+
+            //Act
+            RoomTypePrice roomTypePrice = new RoomTypePrice(date);
+
+            //Assert
+            Assert.Equal(date, roomTypePrice.Date);
+        }
+
+        [Fact]
+        public void PriceSetterThrowsWhenValueIsNegativeTest()
+        {
+            //Arrange
+            DateTime date = DateTime.Now;
+            decimal price = -1;
+            void Action()
+            {
+                RoomTypePrice roomTypePrice = new RoomTypePrice(date, price);
+            }
+
+            //Act and Assert
+            Assert.Throws<ArgumentOutOfRangeException>(Action);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void PriceSetterCorrectlyAssignsValueTest(decimal price)
+        {
+            //Arrange
+            DateTime date = DateTime.Now;
+
+            //Act
+            RoomTypePrice roomTypePrice = new RoomTypePrice(date, price);
+
+            //Assert
+            Assert.Equal(price, roomTypePrice.Price);
         }
 
     }
