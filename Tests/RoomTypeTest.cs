cfa6007ddb4505b70edf87a677de0f7cde0ebb04
Mrtobs <tlaust18@student.aau.dk>
2021-11-17 13:17:48 +0100

    Unit tests updated

--- Tests/RoomTypeTest.cs
+++ Tests/RoomTypeTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using HotelPriceScout.Data.Model;
+using System.Linq;
 
 namespace Tests
 {
@@ -41,7 +42,8 @@ namespace Tests
             //Arrange
             int value = ((DateTime.Now.AddMonths(3).DayOfYear) - DateTime.Now.DayOfYear);
 
-            //Makes sure that if there's a year change, it'll + the days to the total amount
+            //Makes sure that if there's a year change, it'll + the days to the total amount.
+            // It says 366, as the days of the year range from 1-366 instead of 0-365.
             if(value < 0)
             {
                 value = value + 366;
@@ -55,12 +57,40 @@ namespace Tests
         [Fact]
         public void Test_For_Minimum_Date()
         {
-            //Arrange
-
-            //Act
+            //Arrange and Act
+            var value = DateTime.Now.Date;
+            RoomType roomType = new RoomType(1);
 
             //Assert
+            Assert.Equal(roomType.Prices.Min(price => price.Date), value);
         }
 
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [Theory]
+        public void Test_For_No_Null_Prices(int value)
+        {
+            //Arrange and Act
+            RoomType roomType = new RoomType(value);
+
+            //Assert
+            Assert.DoesNotContain(null, roomType.Prices);
+        }
+
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(4)]
+        [Theory]
+        public void Test_For_No_Zero_Prices(int value)
+        {
+            //Arrange and Act
+            RoomType roomType = new RoomType(value);
+            object zero = 0;
+
+
+            //Assert
+            Assert.DoesNotContain(zero, roomType.Prices);
+        }
     }
 }
