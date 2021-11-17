a4f699b2c4a62245f4d68e2024a84441ebe3d270
Mrtobs <tlaust18@student.aau.dk>
2021-11-17 15:31:06 +0100

    Fixed

--- Tests/ScoutTest.cs
+++ Tests/ScoutTest.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using HotelPriceScout.Data.Model;
 
 namespace Tests
 {
     public class ScoutTest
     {
+    [Theory]
+    [InlineData("stopped", -1, null)]
+    [InlineData("test", 55, null)]
+    [InlineData("stopped", 101, null)]
+    public async Task Check_If_MarginValue_Throws(string state, int marginvalue, DateTime[] value)
+        {
+            //Arrange and act
+            async Task ActionAsync()
+            {
+                Scout scout = await Scout.CreateScoutAsync(state, marginvalue, value);
+            }
+
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(ActionAsync);
+        }
+
+
     }
 }
