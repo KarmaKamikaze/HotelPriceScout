4ef6fdd32971b848c9638de51525ccbab0c3ebdf
Mrtobs <tlaust18@student.aau.dk>
2021-11-18 10:17:15 +0100

    More unit test done

--- Tests/ScoutTest.cs
+++ Tests/ScoutTest.cs
@@ -10,21 +10,34 @@ namespace Tests
 {
     public class ScoutTest
     {
-    [Theory]
-    [InlineData("stopped", -1, null)]
-    [InlineData("test", 55, null)]
-    [InlineData("stopped", 101, null)]
-    public async Task Check_If_MarginValue_Throws(string state, int marginvalue, DateTime[] value)
+        [Theory]
+        [InlineData("stopped", -1000, null)]
+        [InlineData("started", -1, null)]
+        [InlineData("started", 101, null)]
+        [InlineData("stopped", 1000, null)]
+        public async Task Check_If_MarginValue_Throws(string state, int marginvalue, DateTime[] value)
         {
             //Arrange and act
             async Task ActionAsync()
             {
                 Scout scout = await Scout.CreateScoutAsync(state, marginvalue, value);
             }
-
+            //Assert
             await Assert.ThrowsAsync<ArgumentOutOfRangeException>(ActionAsync);
         }
 
-
+        [Theory]
+        [InlineData("test", 0, null)]
+        [InlineData("falsk", 55, null)]
+        [InlineData("programmering", 100, null)]
+        public async Task Check_If_State_Throws(string state, int marginvalue, DateTime[] value)
+        {
+            async Task ActionAsync()
+            {
+                Scout scout = await Scout.CreateScoutAsync(state, marginvalue, value);
+            }
+            //Assert
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(ActionAsync);
+        }
     }
 }
