4bf7bcd0be5f8b17bd601683bedcb95f0d19881a
Sarmisuper <sarmilan3@gmail.com>
2021-11-19 14:48:43 +0100

    testssss

--- Tests/PseudoScraperTest.cs
+++ Tests/PseudoScraperTest.cs
@@ -1,11 +1,45 @@
 using System;
+using System.Collections.Generic;
 using HotelPriceScout.Data.Function;
+using HotelPriceScout.Data.Model;
 using Xunit;
 
 namespace Tests
 {
     public class PseudoScraperTest
     {
-        
+
+        [Fact]
+        public void BookingsiteNullConstructorThrowsTest()
+        {
+            //Arrange
+            BookingSite bookingSite = null;
+            void Action()
+            {
+                new PseudoScraper(bookingSite);
+            }
+
+            //Act and Assert
+            Assert.Throws<NullReferenceException>(Action);
+        }
+
+        [Fact]
+        public void BookingsiteAssignedConstructorTest()
+        {
+            //Arrange
+            Dictionary<string, string> hotelStrings = new Dictionary<string, string>()
+            {
+                { "hotel1", "tag1" }
+            };
+            BookingSite bookingSite = new BookingSite("bookingsite", "single", "https://url.com", hotelStrings);
+
+            //Act
+            PseudoScraper pseudoScraper = new PseudoScraper(bookingSite);
+
+            //Assert
+            Assert.Equal(bookingSite, pseudoScraper.BookingSite);
+
+
+        }
     }
 }
