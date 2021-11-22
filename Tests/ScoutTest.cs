974506ff64d4a8670f279a308b7a76b4e7899217
Mrtobs <tlaust18@student.aau.dk>
2021-11-22 11:40:30 +0100

    Fixed test data after correction

--- Tests/ScoutTest.cs
+++ Tests/ScoutTest.cs
@@ -9,9 +9,8 @@ namespace Tests
     {
         [Theory]
         [InlineData(-1000, null)]
+        [InlineData(-500, null)]
         [InlineData(-1, null)]
-        [InlineData(101, null)]
-        [InlineData(1000, null)]
         public async Task Check_If_MarginValue_Throws(int marginvalue, DateTime[] value)
         {
             //Arrange and act
