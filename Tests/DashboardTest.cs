db6738173b24dbe006a2d1a078fea40ca00150bd
Mrtobs <tlaust18@student.aau.dk>
2021-11-22 15:36:40 +0100

    Started tests

---
+++ Tests/DashboardTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class DashboardTest
+    {
+        public void Test_If_SelectedMonthMarketPrices_Returns_Correct_DataList()
+        {
+
+        }
+
+        public void Test_If_GetSingleDayMarketPrice_Returns_Zero()
+        {
+
+        }
+
+        public void Test_If_GetSingleDayMarketPrice_Returns_Price()
+        {
+
+        }
+
+        public void Test_If_RetrieveSelectDataFromDB
+
+    }
+}
---
+++ Tests/SettingsManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using HotelPriceScout.Data.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class SettingsManagerTest
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(4)]
+        [InlineData(-1)]
+        [InlineData(-5)]
+        [InlineData(null)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Check_If_GetNotificationTime_Throws_Error(int value)
+        {
+            //Arrange and Act
+            DateTime dateTime = new DateTime(value);
+
+            void Action()
+            {
+                SettingsManager settingsManager = new SettingsManager();
+                settingsManager.GetNotificationTimes();
+            }
+            //Assert
+            Assert.Throws<ArgumentOutOfRangeException>(Action);
+        }
+
+
+    }
+}
