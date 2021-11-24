db55b8d1d9fa320d39853220ab4feda1a8466b64
Mrtobs <tlaust18@student.aau.dk>
2021-11-24 13:30:18 +0100

    Unit tests for Dashboard done, but still needs UpdateUiMissingDataWarning, as this hasns't been created yet

--- Tests/SettingsManagerTest.cs
+++ Tests/SettingsManagerTest.cs
@@ -10,29 +10,6 @@ namespace Tests
 {
     public class SettingsManagerTest
     {
-        //[Theory]
-        //[InlineData(0)]
-        //[InlineData(4)]
-        //[InlineData(-1)]
-        //[InlineData(-5)]
-        //[InlineData(null)]
-        //[InlineData(1)]
-        //[InlineData(2)]
-        //[InlineData(3)]
-        //public void Check_If_GetNotificationTime_Throws_Error(int value)
-        //{
-        //    //Arrange and Act
-        //    DateTime dateTime = new DateTime(value);
-
-        //    void Action()
-        //    {
-        //        SettingsManager settingsManager = new SettingsManager();
-        //        settingsManager.GetNotificationTimes();
-        //    }
-        //    //Assert
-        //    Assert.Throws<ArgumentOutOfRangeException>(Action);
-        //}
-
 
     }
 }
