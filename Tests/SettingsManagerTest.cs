62e20f028c4b0ec7c9e72c23b22fc3880f855dfa
Mrtobs <tlaust18@student.aau.dk>
2021-11-25 13:40:31 +0100

    Sarmi, i'm gonna kill myself

--- Tests/SettingsManagerTest.cs
+++ Tests/SettingsManagerTest.cs
@@ -24,44 +24,5 @@ namespace Tests
             //Assert
             Assert.Throws<ArgumentOutOfRangeException>(settingsManager.GetNotificationTimes);
         }
-
-        public void Test_If_SetStartScoutSettings_Reverses_Bools()
-        {
-
-        }
-
-        public void Test_If_SetStopScoutSettings_Reverses_Bools()
-        {
-
-        }
-
-        public void Test_If_SetUpdateScoutSettings_Reverses_Bools()
-        {
-
-        }
-
-
-        public static readonly object[][] EscapeUpdateInfo =
-        {
-            new object[] {},
-            new object[] {},
-            new object[] {}
-        };
-
-        
-        public void Test_If_EscapeUpdate_Escapes()
-        {
-
-        }
-
-        public void Test_If_EscapeStop_Escapes()
-        {
-
-        }
-
-
-
-
-
     }
 }
