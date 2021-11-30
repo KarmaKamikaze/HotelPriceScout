01716e35a0a5ffd2bb1f0952ac0d2d7db26f0e39
KarmaKamikaze <nicolaijgn@gmail.com>
2021-11-30 14:41:39 +0100

    SettingsPage E2E tests available content

--- Tests/SettingsPageE2ETests.cs
+++ Tests/SettingsPageE2ETests.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -14,14 +13,14 @@ namespace Tests
         public void SettingsPageShouldNotContainStartButtonIfScoutIsStarted()
         {
             ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddArgument("--ignore-certificate-errors");
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
             using IWebDriver driver = new ChromeDriver(chromeOptions);
             driver.Navigate().GoToUrl("https://localhost:5001/Settings");
             var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
-            IWebElement startButton = wait.Until(element => element.FindElement(By.Id("start-button")));
+            IWebElement startButton = wait.Until(webDriver => webDriver.FindElement(By.Id("start-button")));
             startButton.Click();
-            IWebElement stayButton = wait.Until(element => element.FindElement(By.Id("stay-button")));
+            IWebElement stayButton = wait.Until(webDriver => webDriver.FindElement(By.Id("stay-button")));
             stayButton.Click();
             
             void Action() => driver.FindElement(By.Id("start-button"));
@@ -29,8 +28,95 @@ namespace Tests
             Assert.Throws<NoSuchElementException>(Action);
             
             // Reset shared E2E test state
-            wait.Until(element => element.FindElement(By.Id("stop-button"))).Click();
-            wait.Until(element => element.FindElement(By.Id("confirm-stop-button"))).Click();
+            wait.Until(webDriver => webDriver.FindElement(By.Id("stop-button"))).Click();
+            wait.Until(webDriver => webDriver.FindElement(By.Id("confirm-stop-button"))).Click();
+        }
+        
+        [Fact]
+        public void SettingsPageShouldContainStopButtonIfScoutIsStarted()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            IWebElement startButton = wait.Until(webDriver => webDriver.FindElement(By.Id("start-button")));
+            startButton.Click();
+            IWebElement stayButton = wait.Until(webDriver => webDriver.FindElement(By.Id("stay-button")));
+            stayButton.Click();
+
+            IWebElement stopButton = driver.FindElement(By.Id("stop-button"));
+            
+            Assert.NotNull(stopButton);
+            
+            // Reset shared E2E test state
+            wait.Until(webDriver => webDriver.FindElement(By.Id("stop-button"))).Click();
+            wait.Until(webDriver => webDriver.FindElement(By.Id("confirm-stop-button"))).Click();
+        }
+        
+        [Fact]
+        public void SettingsPageShouldContainUpdateButtonIfScoutIsStarted()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            IWebElement startButton = wait.Until(webDriver => webDriver.FindElement(By.Id("start-button")));
+            startButton.Click();
+            IWebElement stayButton = wait.Until(webDriver => webDriver.FindElement(By.Id("stay-button")));
+            stayButton.Click();
+
+            IWebElement stopButton = driver.FindElement(By.Id("update-button"));
+            
+            Assert.NotNull(stopButton);
+            
+            // Reset shared E2E test state
+            wait.Until(webDriver => webDriver.FindElement(By.Id("stop-button"))).Click();
+            wait.Until(webDriver => webDriver.FindElement(By.Id("confirm-stop-button"))).Click();
+        }
+
+        [Fact]
+        public void SettingsPageShouldContainDashboardRedirectOptionInNavMenu()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            
+            IWebElement homeRedirect = wait.Until(webDriver => webDriver.FindElement(By.Id("nav-home")));
+            
+            Assert.NotNull(homeRedirect);
+        }
+        
+        [Fact]
+        public void SettingsPageShouldNotContainSettingsRedirectOptionInNavMenu()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+
+            void Action() => driver.FindElement(By.Id("nav-settings"));
+            
+            Assert.Throws<NoSuchElementException>(Action);
+        }
+        
+        [Fact]
+        public void SettingsPageShouldContainHomeLogoRedirectOptionInNavMenu()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            
+            IWebElement homeRedirect = wait.Until(webDriver => webDriver.FindElement(By.Id("nav-home")));
+            
+            Assert.NotNull(homeRedirect);
         }
     }
 }
