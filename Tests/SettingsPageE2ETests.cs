d699f40f87497a306e2f3cffe3b1f639bd8afe72
KarmaKamikaze <nicolaijgn@gmail.com>
2021-11-30 15:11:51 +0100

    Tests redirects on settings page

--- Tests/SettingsPageE2ETests.cs
+++ Tests/SettingsPageE2ETests.cs
@@ -69,9 +69,9 @@ namespace Tests
             IWebElement stayButton = wait.Until(webDriver => webDriver.FindElement(By.Id("stay-button")));
             stayButton.Click();
 
-            IWebElement stopButton = driver.FindElement(By.Id("update-button"));
+            IWebElement updateButton = driver.FindElement(By.Id("update-button"));
             
-            Assert.NotNull(stopButton);
+            Assert.NotNull(updateButton);
             
             // Reset shared E2E test state
             wait.Until(webDriver => webDriver.FindElement(By.Id("stop-button"))).Click();
@@ -117,6 +117,98 @@ namespace Tests
             IWebElement homeRedirect = wait.Until(webDriver => webDriver.FindElement(By.Id("nav-home")));
             
             Assert.NotNull(homeRedirect);
+        }
+        
+        [Fact]
+        public void SettingsPageRedirectsToDashboardWhenHomeLogoIsPressedWhenScoutIsRunning()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            
+            IWebElement startButton = wait.Until(webDriver => webDriver.FindElement(By.Id("start-button")));
+            startButton.Click();
+            IWebElement stayButton = wait.Until(webDriver => webDriver.FindElement(By.Id("stay-button")));
+            stayButton.Click();
+
+            IWebElement navLogo = wait.Until(webDriver => webDriver.FindElement(By.Id("nav-logo")));
+            navLogo.Click();
+            
+            string expectedUrl = "https://localhost:5001/Dashboard";
+            bool urlRedirects = wait.Until(webDriver => webDriver.Url == expectedUrl);
+            
+            Assert.True(urlRedirects);
+            
+            // Reset shared E2E test state
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            wait.Until(webDriver => webDriver.FindElement(By.Id("stop-button"))).Click();
+            wait.Until(webDriver => webDriver.FindElement(By.Id("confirm-stop-button"))).Click();
+        }
+        
+        [Fact]
+        public void SettingsPageRedirectsBackToSettingsWhenHomeLogoIsPressedWhenScoutIsNotRunning()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            IWebElement navLogo = wait.Until(webDriver => webDriver.FindElement(By.Id("nav-logo")));
+            navLogo.Click();
+            
+            string expectedUrl = "https://localhost:5001/Settings";
+            bool urlRedirects = wait.Until(webDriver => webDriver.Url == expectedUrl);
+            
+            Assert.True(urlRedirects);
+        }
+        
+        [Fact]
+        public void SettingsPageRedirectsToDashboardWhenHomeNavIsPressedWhenScoutIsRunning()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            
+            IWebElement startButton = wait.Until(webDriver => webDriver.FindElement(By.Id("start-button")));
+            startButton.Click();
+            IWebElement stayButton = wait.Until(webDriver => webDriver.FindElement(By.Id("stay-button")));
+            stayButton.Click();
+
+            IWebElement navLogo = wait.Until(webDriver => webDriver.FindElement(By.Id("nav-home")));
+            navLogo.Click();
+            
+            string expectedUrl = "https://localhost:5001/Dashboard";
+            bool urlRedirects = wait.Until(webDriver => webDriver.Url == expectedUrl);
+            
+            Assert.True(urlRedirects);
+            
+            // Reset shared E2E test state
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            wait.Until(webDriver => webDriver.FindElement(By.Id("stop-button"))).Click();
+            wait.Until(webDriver => webDriver.FindElement(By.Id("confirm-stop-button"))).Click();
+        }
+        
+        [Fact]
+        public void SettingsPageRedirectsBackToSettingsWhenHomeNavIsPressedWhenScoutIsNotRunning()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--ignore-certificate-errors", "headless");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("https://localhost:5001/Settings");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            IWebElement navLogo = wait.Until(webDriver => webDriver.FindElement(By.Id("nav-home")));
+            navLogo.Click();
+            
+            string expectedUrl = "https://localhost:5001/Settings";
+            bool urlRedirects = wait.Until(webDriver => webDriver.Url == expectedUrl);
+            
+            Assert.True(urlRedirects);
         }
     }
 }
