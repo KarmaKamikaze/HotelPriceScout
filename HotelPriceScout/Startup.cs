0af1c283f24d2b1ea599f4e2979971d3b6a7d9c7
KarmaKamikaze <nicolaijgn@gmail.com>
2021-12-01 10:31:29 +0100

    Change SettingsPage from Transient to Singleton

--- HotelPriceScout/Startup.cs
+++ HotelPriceScout/Startup.cs
@@ -28,7 +28,7 @@ namespace HotelPriceScout
             services.AddBlazoredModal();
             services.AddTransient<IDashboard, Dashboard>();
             services.AddTransient<ISqliteDataAccess, SqliteDataAccess>();
-            services.AddTransient<SettingsManager>();
+            services.AddSingleton<SettingsManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
